ff91879f898b5aa59b044b3698fa295ea7ca7a1a
Bethany Huhtala-Schoonover <betheth614@gmail.com>
2021-01-31 12:48:34 -0500

    review exercise pet elevator pair project

--- PetElevator.Tests/CustomerTests.cs
+++ PetElevator.Tests/CustomerTests.cs
@@ -2,11 +2,36 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using PetElevator.HR;
 
 namespace PetElevator.Tests
 {
     [TestClass]
-    class CustomerTests
+    public class CustomerTests
     {
+        [TestMethod]
+        public void CustomerBalanceDue()
+        {
+            Customer customer = new Customer("John", "Smith");
+
+            double balanceDue = 0.5;
+            Dictionary<string, double> services = new Dictionary<string, double>();
+            services.Add("Walking", 0.5);
+
+
+            Assert.AreEqual(balanceDue, customer.GetBalanceDue(services));
+        }
+
+        [TestMethod]
+        public void EmployeeBalanceDue()
+        {
+            Employee employee = new Employee("Jane", "Doe");
+
+            double balanceDue = 0.25;
+            Dictionary<string, double> services = new Dictionary<string, double>();
+            services.Add("Walking", 0.5);
+
+            Assert.AreEqual(balanceDue, employee.GetBalanceDue(services));
+        }
     }
 }
