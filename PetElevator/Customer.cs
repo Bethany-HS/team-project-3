40f20cf0eb7742fe064a5cf1fc74d0a32f191888
Bethany Huhtala-Schoonover <betheth614@gmail.com>
2021-01-31 11:59:48 -0500

    pet project

---
+++ PetElevator/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PetElevator.Shared;
+
+namespace PetElevator
+{
+    public class Customer : Person
+    {
+        public Customer(string firstName, string lastName, string phoneNumber) : base(firstName, lastName)
+        {
+            PhoneNumber = phoneNumber;
+        }
+        public Customer(string firstName, string lastName) : base(firstName, lastName)
+        {
+
+        }
+
+        public string PhoneNumber { get; set; } = "";
+        public List<Pet> Pets { get; set; } = new List<Pet>();
+
+    }
+}
