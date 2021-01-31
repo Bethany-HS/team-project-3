7bd96fa49f14910175a50861f9f556d79ac04a9d
Jeff Staley <staleyjeffrey@gmail.com>
2021-01-31 11:42:48 -0500

    Update 1 to Week 3 Pair Project - Pet Tests Created

---
+++ PetElevator.Tests/PetTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetElevator.Tests
+{
+    [TestClass]
+    public class PetTests
+    {
+        [TestMethod]
+        public void TestForVaccinations()
+        {
+            Pet pet = new Pet("Spot", "dog");
+
+            string testForRabies = "Rabies";
+
+            Assert.AreEqual(testForRabies, pet.ListVaccinations());
+        }
+    }
+}
---
+++ PetElevator/Pet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetElevator
+{
+    public class Pet
+    {
+        public Pet(string petName, string species)
+        {
+            PetName = petName;
+            Species = species;
+        }
+        public string PetName
+        {
+            get;
+            set;
+        }
+        public string Species
+        {
+            get;
+            set;
+        }
+        public List<string> Vaccinations
+        {
+            get;
+            set;
+        } = new List<string>();
+        public string ListVaccinations()
+        {
+            string vaccinationsCSV = "";
+            int i = 0;
+            while (i < Vaccinations.Count)
+            {
+                if (i > 0)
+                {
+                    vaccinationsCSV += ", ";
+                }
+                vaccinationsCSV += Vaccinations[i];
+                i++;
+            }
+            return vaccinationsCSV;
+        }
+    }
+}
